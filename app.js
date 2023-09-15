90d702eb697e099c88058a52dc8a7464f81ce8f0
henry-gu <henryguhb@outlook.com>
2023-09-15 15:52:35 +0800

    showlogs

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ function getUTCDateTime() {
 }
 
 function appendLog(logMessage) {
-  logMessages.unshift(logMessage);
+  //logMessages.unshift(logMessage);
+  logMessages.push(logMessage);
   console.log(logMessage);
 }
 
@@ -38,17 +39,31 @@ function parseJwt(tokenString) {
       return null;
     }
 
-    // Print the decoded token header and payload
-    console.log('Decoded Token Header:');
-    console.log(decodedToken.header);
-    console.log('Decoded Token Payload:');
-    console.log(decodedToken.payload);
+    // // Print the decoded token header and payload
+    // console.log('Decoded Token Header:');
+    // console.log(decodedToken.header);
+    // console.log('Decoded Token Payload:');
+    // console.log(decodedToken.payload);
 
     return decodedToken;
   } catch (error) {
     console.error('An error occurred:', error.message);
     return null;
   }
+}
+
+function unixTimestampToUTC(unixTimestamp) {
+  const utcDate = new Date(unixTimestamp * 1000); // Multiply by 1000 to convert seconds to milliseconds
+  const year = utcDate.getUTCFullYear();
+  const month = utcDate.getUTCMonth() + 1; // Month is 0-based, so add 1
+  const day = utcDate.getUTCDate();
+  const hours = utcDate.getUTCHours();
+  const minutes = utcDate.getUTCMinutes();
+  const seconds = utcDate.getUTCSeconds();
+
+  const utcTimeString = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')} ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  
+  return utcTimeString;
 }
 
 //////////////////////////////////////////////
@@ -65,17 +80,19 @@ app.get("/connect", (req, res) => {
   authUserId = req.query.userId;
 
   const serverAddress = req.hostname;
-  const queryUrl = req.url.split('?')[0];
+  // const queryUrl = req.url.split('?')[0];
+  const queryUrl = req.url;
 
-  appendLog(`${getUTCDateTime()} >>> Load the landing page.`);
-  appendLog(`${getUTCDateTime()} >>> Server Address=${serverAddress}`);
-  appendLog(`${getUTCDateTime()} >>> Query endpoint=${queryUrl}`);
-
-
-  appendLog(`${getUTCDateTime()} >>> Query Parameters:`);
-  appendLog(`${getUTCDateTime()} >>> id=${authId}`);
-  appendLog(`${getUTCDateTime()} >>> requestToken=${authRequestToken}`);
-  appendLog(`${getUTCDateTime()} >>> userId=${authUserId}`);
+  appendLog(`========================================================================`);
+  appendLog(`${getUTCDateTime()} >>> REDIRECT TO THE LANDING PAGE`);
+  appendLog(`------------------------------------------------------------------------`);
+  appendLog(`Server Address=${serverAddress}`);
+  appendLog(`Query URL=${queryUrl}`);
+  appendLog(`Query Parameters:`);
+  appendLog(`-- id=${authId}`);
+  appendLog(`-- requestToken=${authRequestToken}`);
+  appendLog(`-- userId=${authUserId}`);
+  appendLog(``);
 
   res.render("connect");
 });
@@ -86,23 +103,43 @@ app.post("/auth", async (req, res) => {
 
   // Get the "account-id" and "auth-code" from the request body
   const { 'account-id': accountId, 'auth-code': accountAuthCode } = req.body;
-  appendLog(`${getUTCDateTime()} >>> Account ID: ${accountId}`);
-  appendLog(`${getUTCDateTime()} >>> Auth Code: ${accountAuthCode}`);
+  appendLog(`========================================================================`);
+  appendLog(`${getUTCDateTime()} >>> AUTHORIZE COMPANY IDENTITY`);
+  appendLog(`------------------------------------------------------------------------`);
+  appendLog(`Account ID: ${accountId}`);
+  appendLog(`Auth Code: ${accountAuthCode}`);
+  appendLog(``);
 
   // Check if authCode ends with '0000'
   if (accountId && accountAuthCode.endsWith('0000')) {
-    console.error(`${getUTCDateTime()} >>> User Authorization Failed. Auth code ends with '0000'`);
+    appendLog(`========================================================================`);
+    appendLog(`${getUTCDateTime()} >>> COMPANY AUTHORIZATION FAILED`);
+    appendLog(`------------------------------------------------------------------------`);
+    appendLog(`Auth code should not end with '0000'`);
+    appendLog(``);
+
     res.redirect("/failure");
     return; // Exit the function
   } else {
-    appendLog(`${getUTCDateTime()} >>> User Authorization Success.`);
+    appendLog(`========================================================================`);
+    appendLog(`${getUTCDateTime()} >>> COMPANY AUTHORIZATION SUCCESS`);
+    appendLog(`------------------------------------------------------------------------`);
+    appendLog(``);
   }
 
-  appendLog(`${getUTCDateTime()} >>> Call Concur Auth API`);
-  appendLog(`${getUTCDateTime()} >>> grant_type=password`);
-  appendLog(`${getUTCDateTime()} >>> credtype=authtoken`);
-  appendLog(`${getUTCDateTime()} >>> username=${authId}`);
-  appendLog(`${getUTCDateTime()} >>> password=${authRequestToken}`);
+  appendLog(`========================================================================`);
+  appendLog(`${getUTCDateTime()}  >>> CALL OAUTH2 API TO REQUEST TOKEN`);
+  appendLog(`------------------------------------------------------------------------`);
+  appendLog(`Method="POST"`);
+  appendLog(`URL="https://us.api.concursolutions.com/oauth2/v0/token"`);
+  appendLog(`Content-Type=""application/x-www-form-urlencoded"`);
+  appendLog(`-- client_id=${clientId.slice(0,16)}${"x".repeat(20)}`);
+  appendLog(`-- client_secret=${"x".repeat(36)}`);
+  appendLog(`-- grant_type="password"`);
+  appendLog(`-- redtype="authtoken"`);
+  appendLog(`-- username="${authId}"`);
+  appendLog(`-- password="${authRequestToken}"`);
+  appendLog(``);
 
   const request = require("request");
   const options = {
@@ -124,24 +161,42 @@ app.post("/auth", async (req, res) => {
   try {
     request(options, function (error, response) {
       if (error) {
-        console.error(`${getUTCDateTime()} >>> Error: ${error.message}`);
+        appendLog(`========================================================================`);
+        appendLog(`${getUTCDateTime()} >>> ERROR: CALL OAUTH2 API`);
+        appendLog(`------------------------------------------------------------------------`);
+        console.error(`Error: ${error.message}`);
         res.redirect("/failure");
         return;
       }
-      appendLog(`${getUTCDateTime()} >>> Concur-Correlationid: ${response.headers["concur-correlationid"]}`);
-      appendLog(`${getUTCDateTime()} >>> Response Body:`);
+
+      appendLog(`========================================================================`);
+      appendLog(`${getUTCDateTime()} >>> CALL OAUTH2 API SUCCESS`);
+      appendLog(`------------------------------------------------------------------------`);
+      appendLog(`Response Status: ${response.statusCode}`);
+      appendLog(`Response Header:`);
+      appendLog(`"concur-correlationid": ${response.headers["concur-correlationid"]}`);
+      appendLog(`Response Body:`);
       const responseJson = JSON.parse(response.body);
-      appendLog(`${getUTCDateTime()} --- expires_in: ${responseJson.expires_in}`);
-      appendLog(`${getUTCDateTime()} --- scope: ${responseJson.scope}`);
-      appendLog(`${getUTCDateTime()} --- token_type: ${responseJson.token_type}`);
-      appendLog(`${getUTCDateTime()} --- refresh_token: ${responseJson.refresh_token}`);
-      appendLog(`${getUTCDateTime()} --- refresh_token_expires_in: ${responseJson.refresh_token_expires_in}`);
-      appendLog(`${getUTCDateTime()} --- geolocation: ${responseJson.geolocation}`);
-      appendLog(`${getUTCDateTime()} --- access_token: ${responseJson.access_token}`);      
-      const accessJwtToken = JSON.stringify(parseJwt(responseJson.access_token),null,4);
-      appendLog(`${getUTCDateTime()} --- decoded access-token: ${accessJwtToken}`);
-      const idJwtToken = JSON.stringify(parseJwt(responseJson.id_token),null,4);;
-      appendLog(`${getUTCDateTime()} --- decoded id_token: ${idJwtToken}`);
+      appendLog(`"expires_in": ${responseJson.expires_in}`);
+      appendLog(`"scope": ${responseJson.scope}`);
+      appendLog(`"token_type": ${responseJson.token_type}`);
+      appendLog(`"refresh_token": ${responseJson.refresh_token}`);
+      const expireTimeUTC = unixTimestampToUTC(responseJson.refresh_expires_in);
+      appendLog(`"refresh_expires_in": ${responseJson.refresh_expires_in} (UTC: ${expireTimeUTC})`);
+      appendLog(`"geolocation": ${responseJson.geolocation}`);
+      const accessToken = responseJson.access_token;
+      appendLog(`"access_token":${accessToken.slice(0, 30)}${".".repeat(20)}${accessToken.slice(-20)}`);
+      const idToken = responseJson.id_token;
+      appendLog(`"id_token": ${idToken.slice(0,30)}${".".repeat(20)}${idToken.slice(-20)}`);
+      appendLog(`************************************************************************`);
+      const accessJwtTokenPayload = JSON.stringify(parseJwt(responseJson.access_token), null, 4);
+      appendLog(`<JWT decoded access-token >:`);
+      appendLog(`${accessJwtTokenPayload}`);
+      appendLog(`************************************************************************`);
+      const idJwtTokenPayload = JSON.stringify(parseJwt(responseJson.id_token), null, 4);;
+      appendLog(`<JWT decoded id_token >:`);
+      appendLog(`${idJwtTokenPayload}`);
+      appendLog(``);
 
       res.redirect("/success");
     });
@@ -159,14 +214,21 @@ app.post("/auth", async (req, res) => {
 //////////////////////////////////////////////
 // AUTHORIZE SUCCESS PAGE
 app.get("/success", (req, res) => {
-  appendLog(`${getUTCDateTime()} >>> Redirect to Connection Success Page.`);
+  appendLog(`========================================================================`);
+  appendLog(`${getUTCDateTime()}  >>> REDIRECT TO SUCCESS MESSAGE PAGE`);
+  appendLog(`------------------------------------------------------------------------`);
+  appendLog(``);
   res.render("success");
 });
 
 //////////////////////////////////////////////
 // AUTHORIZE FAILURE PAGE
 app.get("/failure", (req, res) => {
-  appendLog(`${getUTCDateTime()} >>> Redirect To Show Connection Failure Page.`);
+  appendLog(`========================================================================`);
+  appendLog(`${getUTCDateTime()}  >>> REDIRECT TO FAILURE MESSAGE PAGE`);
+  appendLog(`------------------------------------------------------------------------`);
+  appendLog(``);
+
   res.render("failure");
 });
 
